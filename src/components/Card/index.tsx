4b2bb8945e5602fbbf309a8f57c1ecf049069403
rodrigocelmer <rodrigocelmer@hotmail.com>
2022-07-23 16:24:07 -0300

    wip - card hero

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export default function CardImgMedia() {
+  return (
+    <Card sx={{ maxWidth: '50vh' }}>
+      <CardMedia
+        component="img"
+        alt="3-D Man"
+        height="140"
+        image="http://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          3-D Man
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+            Presente em 12 comics (listar alguns exemplos?)
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Link para Comics - API retorna</Button>
+        <Button size="small">Link para Wiki - API retorna</Button>
+      </CardActions>
+    </Card>
+  );
+}
--- src/pages/CardHeroes/index.tsx
+++ src/pages/CardHeroes/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Grid } from "@mui/material";
 import { useState } from "react";
+import CardImgMedia from "../../components/Card";
 import Table from "../../components/Table";
 import TextInput from "../../components/TextInput";
 import DATA from '../../MockData/MOCK_DATA.json'
@@ -13,7 +14,6 @@ export default function Home(){
 
     return(
         <>
-            <h1>HOME</h1>
             <Grid container spacing={2}>
                 <Grid item display='flex' alignItems='center' flexDirection='column' xs={6}>
                     <TextInput title="SEARCH" handleChange={handleChange} searchTerm={searchTerm}/>
@@ -21,7 +21,7 @@ export default function Home(){
                 </Grid>
                 <Grid item xs={6}>
                     <Box display='flex' alignItems='center' justifyContent='center' sx={{height: '80vh'}}>
-                        <h2>Card do hero</h2>
+                        <CardImgMedia/>
                     </Box>
                 </Grid>
             </Grid>
