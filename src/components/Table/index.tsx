f2fef9fe52a4ce715ee338785872f6e7b553123c
rodrigocelmer <rodrigocelmer@hotmail.com>
2022-07-23 16:40:07 -0300

    wip - button on table

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -5,6 +5,7 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import { Box, Button } from '@mui/material';
 
 type Hero = {
   first_name: string
@@ -23,11 +24,18 @@ export default function SimpleTable(props: TableProps) {
       <Table size="small">
         <TableBody>
           {sortedHeroes.map((hero) => (
-            <TableRow
+            <TableRow 
               key={hero.first_name}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell align="center">{hero.first_name}</TableCell>
+              <Box sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column'
+              }}>
+                <Button variant='text' sx={{width: '100%', color: 'black'}}>{hero.first_name}</Button>
+              </Box>
             </TableRow>
           ))}
         </TableBody>
